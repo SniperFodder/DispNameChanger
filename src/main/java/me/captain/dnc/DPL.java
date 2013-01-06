7b85190496537fa07c6f3ca42937c85b9467d974
Mark 'SniperFodder' Gunnett <mgunnett4318@silicateillusion.org>
2013-01-06 11:16:29 -0700

    Converted project to Gradle Directory Structure.

---
+++ src/main/java/me/captain/dnc/DPL.java
@@ -0,0 +1,457 @@
+package me.captain.dnc;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.TimerTask;
+import java.util.logging.Logger;
+
+import me.captain.dnc.DispNameAPI.MessageType;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.getspout.spoutapi.player.SpoutPlayer;
+
+/**
+ * Contains functions for ensuring the player is named properly, as well as
+ * changing kick/death messages. Command PreProcessing is also done here;
+ * This is where display names are replaced in command strings.
+ * 
+ * @author captainawesome7, itsatacoshop247, Daxiongmao87, Luke Zwekii,
+ *         Sammy, SniperFodder
+ * 
+ */
+public class DPL implements Listener
+{
+	private static final Logger log = Bukkit.getLogger();
+	
+	private DispNameChanger plugin;
+	
+	private DispNameAPI api;
+	
+	/**
+	 * Constructs a new DispNameChange Player Listener.
+	 * 
+	 * @param plugin
+	 *            the plugin that created this listener.
+	 */
+	public DPL()
+	{
+		plugin = DispNameChanger.getInstance();
+		
+		api = DispNameAPI.getInstance();
+	}
+	
+	/**
+	 * Triggered on player death.
+	 * 
+	 * @param event
+	 *            the event triggered by the death.
+	 */
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onPlayerDeath(final PlayerDeathEvent event)
+	{
+		if (!plugin.changeDeath())
+			return;
+		
+		Player player = event.getEntity();
+		
+		if (!api.isNameChanged(player))
+			return;
+		
+		event.setDeathMessage(event.getDeathMessage().replaceFirst(
+				player.getName(), player.getDisplayName()));
+	}
+	
+	/**
+	 * Triggered whenever a player has joined the server.
+	 * 
+	 * @param event
+	 */
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onPlayerJoin(final PlayerJoinEvent event)
+	{
+		Player player = event.getPlayer();
+		
+		api.restoreNick(player);
+		
+		plugin.getOrderedPlayers().put(player.getName(), player);
+		
+		if (plugin.changeLogin())
+		{
+			if (api.isNameChanged(player))
+			{
+				event.setJoinMessage(event.getJoinMessage().replaceFirst(
+						player.getName(), player.getDisplayName()));
+			}
+		}
+		
+		if (plugin.isSpoutEnabled())
+		{
+			SpoutPlayer spoutTarget = (SpoutPlayer) player;
+			
+			spoutTarget.setTitle(player.getDisplayName());
+			
+			plugin.getServer()
+					.getScheduler()
+					.scheduleSyncDelayedTask(plugin, new RenameTask(player),
+							1L);
+		}
+	}
+	
+	/**
+	 * Triggered whenever a player is kicked from the server.
+	 * 
+	 * @param event
+	 *            The event triggered by the action.
+	 */
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onPlayerKick(final PlayerKickEvent event)
+	{
+		if (event.isCancelled())
+		{
+			return;
+		}
+		
+		log.info("onPlayerKick - before:" + event.getLeaveMessage());
+		
+		Player player = event.getPlayer();
+		
+		savePlayer(player);
+		
+		if (plugin.changeKick())
+		{
+			if (api.isNameChanged(player))
+			{
+				event.setLeaveMessage(event.getLeaveMessage().replaceFirst(
+						player.getName(), player.getDisplayName()));
+			}
+		}
+	}
+	
+	/**
+	 * Called whenever the player quits the server.
+	 * 
+	 * @param event
+	 *            the event triggered by the action.
+	 */
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onPlayerQuit(final PlayerQuitEvent event)
+	{
+		Player player = event.getPlayer();
+		
+		savePlayer(player);
+		
+		if (plugin.changeLogin())
+		{
+			if (api.isNameChanged(player))
+			{
+				event.setQuitMessage(event.getQuitMessage().replaceFirst(
+						player.getName(), player.getDisplayName()));
+			}
+		}
+	}
+	
+	/**
+	 * Changes any display names to real names in any commands a user sends
+	 * to the server.
+	 * 
+	 * @param event
+	 *            The event that was triggered by sending a command.
+	 */
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onCommandPreprocess(PlayerCommandPreprocessEvent event)
+	{
+		/*
+		 * TODO This does not handle Console Events. Re-write to handle
+		 * ServerCommandEvent.
+		 */
+		Player player = event.getPlayer();
+		
+		StringBuilder sbCommand = null;
+		
+		Player[] target;
+		
+		String[] args = event.getMessage().split(" ");
+		
+		// Ensure that we aren't matching our command.
+		if (args[0].startsWith("/"))
+		{
+			if (checkCommands(args[0]))
+			{
+				return;
+			}
+		}
+		
+		switch (args.length)
+		{
+		// Whatever it is, it can't have a DispName
+		case 1:
+			break;
+		// Possible command + DispName
+		case 2:
+			target = api.checkName(args[1]);
+			
+			switch (target.length)
+			{
+			case 0:
+				break;
+			case 1:
+				if (!target[0].getName().equalsIgnoreCase(args[1]))
+				{
+					sbCommand = new StringBuilder();
+					
+					sbCommand.append(args[0]).append(" ")
+							.append(target[0].getName());
+					
+					break;
+				}
+				break;
+			default:
+				event.setCancelled(true);
+				
+				api.sendMessage(DNCStrings.ERROR_MULTI_MATCH, player, null,
+						MessageType.ERROR);
+			}
+			break;
+		// Possible Command + Multipart DispName
+		default:
+			String[] saArgs = event.getMessage().split(" ", 2);
+			
+			String[] saParsed = api.parseArgumentsAll(saArgs[1]);
+			
+			boolean bNamesAdded = false;
+			
+			int iReplaceMax = checkFiltered(saArgs[0]);
+			
+			int iReplaceCount = 0;
+			
+			sbCommand = new StringBuilder();
+			
+			sbCommand.append(args[0]).append(" ");
+			
+			for (String s : saParsed)
+			{
+				target = api.checkName(s);
+				
+				if (target.length == 0)
+				{
+					sbCommand.append(s).append(" ");
+					
+					continue;
+				}
+				
+				switch (target.length)
+				{
+				case 1:
+					
+					if (!target[0].getName().equalsIgnoreCase(s))
+					{
+						if (iReplaceMax > -1)
+						{
+							if (iReplaceCount < iReplaceMax)
+							{
+								sbCommand.append(target[0].getName()).append(
+										" ");
+								
+								iReplaceCount++;
+							}
+							else
+							{
+								sbCommand.append(s).append(" ");
+							}
+						}
+						else
+						{
+							sbCommand.append(target[0].getName()).append(" ");
+						}
+						
+						bNamesAdded = true;
+					}
+					else
+					{
+						sbCommand.append(s).append(" ");
+					}
+					break;
+				default:
+					
+					event.setCancelled(true);
+					
+					api.sendMessage(DNCStrings.ERROR_MULTI_MATCH, player,
+							null, MessageType.ERROR);
+					
+					sbCommand = null;
+					
+					bNamesAdded = false;
+					
+					break;
+				}
+			}
+			
+			if (bNamesAdded == false)
+			{
+				sbCommand = null;
+			}
+		}
+		
+		if (sbCommand != null)
+		{
+			event.setCancelled(true);
+			
+			player.chat(sbCommand.toString());
+			
+			StringBuilder sb = new StringBuilder();
+			
+			sb.append(DNCStrings.dnc_long).append("Converted |")
+					.append(event.getMessage()).append("| to |")
+					.append(sbCommand.toString()).append("|.");
+			
+			log.info(sb.toString());
+		}
+	}
+	
+	/**
+	 * Checks to see if a command matches one of the current plugin
+	 * commands, or a command we should not filter as indicated by the
+	 * configuration file.
+	 * 
+	 * @param command
+	 *            the command to check for.
+	 * 
+	 * @return true if the command matches, false otherwise.
+	 */
+	private boolean checkCommands(String command)
+	{
+		String sCommand;
+		
+		if (command.startsWith("/"))
+		{
+			sCommand = command.substring(1);
+		}
+		else
+		{
+			sCommand = command;
+		}
+		
+		List<String> aliases;
+		
+		for (DNCCommands cmd : DNCCommands.values())
+		{
+			if (cmd.getName().equalsIgnoreCase(sCommand))
+			{
+				return true;
+			}
+			
+			aliases = plugin.getCommand(cmd.getName()).getAliases();
+			
+			for (String s : aliases)
+			{
+				if (s.equalsIgnoreCase(sCommand))
+				{
+					return true;
+				}
+			}
+		}
+		
+		HashMap<String, Integer> cmdFilter = plugin.getCommandList();
+		
+		for (String s : cmdFilter.keySet())
+		{
+			if (s.equalsIgnoreCase(sCommand) && cmdFilter.get(s) == 0)
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Checks to see if the given command is in the filtered list.
+	 * 
+	 * @param command
+	 * @return
+	 */
+	private int checkFiltered(String command)
+	{
+		String sCommand;
+		
+		if (command.startsWith("/"))
+		{
+			sCommand = command.substring(1, command.length());
+		}
+		else
+		{
+			sCommand = command;
+		}
+		
+		HashMap<String, Integer> cmdFilter = plugin.getCommandList();
+		
+		for (String s : cmdFilter.keySet())
+		{
+			if (s.equalsIgnoreCase(sCommand))
+			{
+				return cmdFilter.get(s);
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * Remove a player that was in the ordered list.
+	 * 
+	 * @param p
+	 *            the player to remove.
+	 */
+	private void removeOrderedPlayer(Player p)
+	{
+		if (plugin.getOrderedPlayers().containsKey(p.getName()))
+		{
+			plugin.getOrderedPlayers().remove(p.getName());
+		}
+	}
+	
+	/**
+	 * Saves a players name on save if Saving on quit is enabled.
+	 * 
+	 * @param player
+	 */
+	private void savePlayer(Player player)
+	{
+		if (plugin.isSaveOnQuit())
+		{
+			api.storeNick(player);
+		}
+		
+		removeOrderedPlayer(player);
+	}
+	
+	/**
+	 * Handles setting the title for everyone when a new player joins.
+	 * 
+	 * @author Mark 'SniperFodder' Gunnett
+	 * 
+	 */
+	private class RenameTask extends TimerTask
+	{
+		private SpoutPlayer player;
+		
+		public RenameTask(Player player)
+		{
+			this.player = (SpoutPlayer) player;
+		}
+		
+		@Override
+		public void run()
+		{
+			player.setTitle(player.getDisplayName());
+		}
+	}
+}
---
+++ src/main/java/me/captain/dnc/DispNameCE.java
@@ -0,0 +1,648 @@
+package me.captain.dnc;
+
+import java.util.Collection;
+import java.util.logging.Logger;
+
+import me.captain.dnc.DispNameAPI.MessageType;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * Command executor for the DispNameChanger Plugin.
+ * 
+ * Valid commands:aliases that this executor handles are
+ * <code>'rename':'newname'</code>, <code>'reset':'resetname'</code>, and
+ * <code>'check':'checkname', 'namecheck', 'realname'</code>.
+ * 
+ * @author captainawesome7, itsatacoshop247, Daxiongmao87, Luke Zwekii,
+ *         Sammy, SniperFodder
+ * 
+ */
+public class DispNameCE implements CommandExecutor
+{
+	
+	private static final Logger log = Bukkit.getLogger();
+	
+	private DispNameChanger plugin;
+	
+	private DispNameAPI api;
+	
+	/**
+	 * Constructs a new DispNameCE.
+	 * 
+	 * @param plugin
+	 *            The plugin using this Command Executor.
+	 */
+	public DispNameCE()
+	{
+		super();
+		
+		plugin = DispNameChanger.getInstance();
+		
+		api = DispNameAPI.getInstance();
+	}
+	
+	/**
+	 * Handles checking of a player's name.
+	 * 
+	 * @param sender
+	 *            the entity that executed the command.
+	 * 
+	 * @param args
+	 *            the args given with the command.
+	 * 
+	 * @return True if a valid command was given.
+	 */
+	public boolean check(CommandSender sender, String[] args)
+	{
+		String[] saArgs = api.parseArguments(args);
+		
+		// No Name to check for.
+		if (saArgs.length == 0)
+		{
+			return false;
+		}
+		// Name Given to check for.
+		else if (saArgs.length == 1)
+		{
+			Player[] players = api.checkName(saArgs[0]);
+			
+			if (players.length == 0)
+			{
+				api.sendMessage(DNCStrings.ERROR_BAD_USER, sender, null,
+						MessageType.ERROR);
+				
+				return true;
+			}
+			else if (players.length == 1)
+			{
+				String sName = players[0].getName();
+				
+				String sDisplay = players[0].getDisplayName();
+				
+				sDisplay = api.stripPrefix(sDisplay);
+				
+				sDisplay = ChatColor.stripColor(sDisplay);
+				
+				Object[] users = new Object[2];
+				
+				if (sDisplay.equals(saArgs[0]))
+				{
+					users[0] = (players[0].getDisplayName());
+					
+					users[1] = sName;
+				}
+				else
+				{
+					users[0] = sName;
+					
+					users[1] = (players[0].getDisplayName());
+				}
+				
+				api.sendMessage(DNCStrings.INFO_CHECK_SINGLE, sender, users,
+						MessageType.INFO);
+				
+				return true;
+			}
+			else
+			{
+				Object[] users = new Object[2];
+				
+				users[0] = saArgs[0];
+				
+				String sUsers = "";
+				
+				for (Player p : players)
+				{
+					sUsers += p.getName() + ", ";
+				}
+				
+				sUsers = sUsers.substring(0, sUsers.lastIndexOf(','));
+				
+				users[1] = sUsers;
+				
+				api.sendMessage(DNCStrings.INFO_CHECK_MULTI, sender, null,
+						MessageType.CONFIRMATION);
+				
+				api.sendMessage(DNCStrings.INFO_CHECK_MULTI_LIST, sender,
+						users, MessageType.INFO);
+				
+				return true;
+			}
+		}
+		else if (saArgs.length > 1)
+		{
+			api.sendMessage(DNCStrings.ERROR_BAD_ARGS, sender, null,
+					MessageType.ERROR);
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Handles listing of user with changed display names. Has pagination.
+	 * 
+	 * @param sender
+	 *            the entity that executed the command.
+	 * 
+	 * @param args
+	 *            the args given with the command.
+	 * 
+	 * @return True if a valid command was given.
+	 */
+	public boolean list(CommandSender sender, String[] args)
+	{
+		Object[] oArgs = new Object[4];
+		
+		Object[] oNames = new Object[2];
+		
+		Player[] aPlayers;
+		
+		int iPageNumber;
+		
+		int iMaxPages;
+		
+		int iStartIndex;
+		
+		if (plugin.getPagination() == 0)
+		{
+			Player[] players = getPlayerList();
+			
+			for (Player p : players)
+			{
+				if (!p.getName().equals(p.getDisplayName()))
+				{
+					oNames[0] = p.getName();
+					
+					oNames[1] = p.getDisplayName();
+					
+					api.sendMessage(DNCStrings.INFO_CHECK_SINGLE, sender,
+							oNames, MessageType.INFO);
+				}
+			}
+			
+			return true;
+		}
+		
+		// Get the page Number
+		if (args.length == 0)
+		{
+			iPageNumber = 1;
+		}
+		else if (args.length > 1)
+		{
+			api.sendMessage(DNCStrings.ERROR_BAD_ARGS, sender, null,
+					MessageType.ERROR);
+			
+			return false;
+		}
+		else
+		{
+			try
+			{
+				iPageNumber = Integer.parseInt(args[0]);
+			}
+			catch (NumberFormatException e)
+			{
+				api.sendMessage(DNCStrings.ERROR_BAD_INPUT, sender, null,
+						MessageType.ERROR);
+				
+				return false;
+			}
+			
+			if (iPageNumber <= 0)
+			{
+				iPageNumber = 1;
+			}
+		}
+		
+		aPlayers = getPlayerList();
+		
+		// Get the page Variables
+		iMaxPages = getMaxPages(aPlayers.length);
+		
+		iStartIndex = getMinPlayerIndex(iPageNumber, iMaxPages);
+		
+		// Ensure we don't exceed max pages.
+		if (iPageNumber >= iMaxPages)
+		{
+			iPageNumber = iMaxPages;
+		}
+		
+		oArgs[0] = "" + iPageNumber;
+		
+		oArgs[1] = "" + iMaxPages;
+		
+		oArgs[2] = DNCCommands.LIST.getName();
+		
+		oArgs[3] = "" + (iPageNumber + 1);
+		
+		if (iPageNumber >= iMaxPages)
+		{
+			api.sendMessage(DNCStrings.INFO_LIST_MAX, sender, oArgs,
+					MessageType.CONFIRMATION);
+		}
+		else
+		{
+			api.sendMessage(DNCStrings.INFO_LIST, sender, oArgs,
+					MessageType.CONFIRMATION);
+		}
+		
+		for (int iLoop1 = iStartIndex; iLoop1 <= ((iStartIndex + plugin
+				.getPagination()) - 1); iLoop1++)
+		{
+			if (iLoop1 > (aPlayers.length - 1))
+			{
+				break;
+			}
+			
+			oNames[0] = aPlayers[iLoop1].getName();
+			
+			oNames[1] = aPlayers[iLoop1].getDisplayName();
+			
+			api.sendMessage(DNCStrings.INFO_CHECK_SINGLE, sender, oNames,
+					MessageType.INFO);
+		}
+		
+		return true;
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd,
+			String commandLabel, String[] args)
+	{
+		// Check for the rename command.
+		if (cmd.getName().equalsIgnoreCase(DNCCommands.RENAME.getName()))
+		{
+			return rename(sender, args);
+		}
+		// Reset name command.
+		else if (cmd.getName().equalsIgnoreCase(DNCCommands.RESET.getName()))
+		{
+			return reset(sender, args);
+		}
+		// Check name command.
+		else if (cmd.getName().equalsIgnoreCase(DNCCommands.CHECK.getName()))
+		{
+			return check(sender, args);
+		}
+		// List Names command
+		else if (cmd.getName().equalsIgnoreCase(DNCCommands.LIST.getName()))
+		{
+			return list(sender, args);
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Handles renaming of users on the server with the given name.
+	 * 
+	 * @param sender
+	 *            the entity that executed the command.
+	 * 
+	 * @param args
+	 *            the args given with the command.
+	 * 
+	 * @return True if a valid command was given.
+	 */
+	public boolean rename(CommandSender sender, String[] args)
+	{
+		// parse Arguments into <target> <Name>
+		String[] saArgs = api.parseArguments(args);
+		
+		// No arguments given. Return false.
+		if (saArgs.length == 0)
+		{
+			return false;
+		}
+		
+		switch (saArgs.length)
+		{
+		// Target = Self
+		case 1:
+			// Ensure we aren't console.
+			if (api.isConsole(sender))
+			{
+				api.sendMessage(DNCStrings.ERROR_CONSOLE_RENAME, sender, null,
+						MessageType.ERROR);
+				
+				return true;
+			}
+			
+			// Check for colors we aren't allowed to use.
+			ChatColor badColor = api.checkForIllegalColors((Player) sender,
+					saArgs[0]);
+			
+			if (badColor != null)
+			{
+				Object[] obj =
+				{ badColor };
+				
+				if (badColor.isColor())
+				{
+					api.sendMessage(DNCStrings.PERMISSION_COLOR, sender, obj,
+							MessageType.ERROR);
+				}
+				else
+				{
+					api.sendMessage(DNCStrings.PERMISSION_STYLE, sender, obj,
+							MessageType.ERROR);
+				}
+				
+				return true;
+			}
+			
+			// Ensure that we are allowed to use Spaces.
+			if (saArgs[0].contains(" "))
+			{
+				if (!api.canUseChangeNameSpace(sender))
+				{
+					api.sendMessage(DNCStrings.PERMISSION_SPACES, sender,
+							null, MessageType.ERROR);
+					
+					return true;
+				}
+			}
+			
+			// Attempt to Change Name.
+			try
+			{
+				if (sender instanceof Player)
+				{
+					api.changeDisplayName((Player) sender, saArgs[0]);
+				}
+				else
+				{
+					api.sendMessage(DNCStrings.ERROR_CONSOLE_RENAME, sender,
+							null, MessageType.ERROR);
+				}
+				
+				return true;
+			}
+			catch (NonUniqueNickException e)
+			{
+				api.sendMessage(DNCStrings.ERROR_NON_UNIQUE, sender, null,
+						MessageType.ERROR);
+				
+				return true;
+			}
+			
+			// Target = Other
+		case 2:
+			// Pull the target we are changing.
+			Player[] players = api.checkName(saArgs[0]);
+			
+			if (players.length == 0)
+			{
+				api.sendMessage(DNCStrings.ERROR_BAD_USER, sender, null,
+						MessageType.ERROR);
+				
+				return true;
+			}
+			// Target Match Found
+			else if (players.length == 1)
+			{
+				if ((sender instanceof Player)
+						&& !players[0].equals((Player) sender))
+				{
+					if (!api.canUseChangeNameOther(sender))
+					{
+						api.sendMessage(DNCStrings.PERMISSION_OTHER, sender,
+								null, MessageType.ERROR);
+						
+						return true;
+					}
+				}
+				if (saArgs[1].contains(" "))
+				{
+					if (!api.canUseChangeNameSpace(sender))
+					{
+						api.sendMessage(DNCStrings.PERMISSION_SPACES, sender,
+								null, MessageType.ERROR);
+						
+						return true;
+					}
+				}
+				
+				try
+				{
+					api.changeDisplayName(sender, players[0], saArgs[1]);
+					
+					return true;
+				}
+				catch (NonUniqueNickException e)
+				{
+					api.sendMessage(DNCStrings.ERROR_NON_UNIQUE, sender, null,
+							MessageType.ERROR);
+					
+					return true;
+				}
+			}
+			else if (players.length > 1)
+			{
+				api.sendMessage(DNCStrings.ERROR_MULTI_MATCH, sender, null,
+						MessageType.ERROR);
+				
+				return true;
+			}
+			
+			break;
+		
+		// Default Case: Should never occur.
+		default:
+			log.severe("API.parseArguments(String[]) returned more than 2 arguments!"
+					+ " Inform the Author of this error!");
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Resets a targets name to their login name.
+	 * 
+	 * @param sender
+	 *            the entity that executed the command.
+	 * 
+	 * @param args
+	 *            the args given with the command.
+	 * 
+	 * @return True if a valid command was given.
+	 */
+	public boolean reset(CommandSender sender, String[] args)
+	{
+		String[] saArgs = api.parseArguments(args);
+		
+		// Reset = Self
+		if (saArgs.length == 0)
+		{
+			if (api.isConsole(sender))
+			{
+				api.sendMessage(DNCStrings.ERROR_CONSOLE_RENAME, sender, null,
+						MessageType.ERROR);
+				
+				return true;
+			}
+			else
+			{
+				Player p = (Player) sender;
+				
+				try
+				{
+					api.changeDisplayName(p, p.getName());
+					
+					return true;
+				}
+				catch (NonUniqueNickException e)
+				{
+					log.severe("There was an error reverting from the DisplayName: Display - "
+							+ p.getDisplayName()
+							+ " | BadNick: "
+							+ e.getBadName());
+					
+					return true;
+				}
+			}
+		}
+		// Reset = Other
+		else if (saArgs.length == 1)
+		{
+			if (!api.canUseChangeNameOther(sender))
+			{
+				api.sendMessage(DNCStrings.PERMISSION_OTHER, sender, null,
+						MessageType.ERROR);
+				
+				return true;
+			}
+			
+			Player[] players = api.checkName(saArgs[0]);
+			
+			switch (players.length)
+			{
+			// No User Matched
+			case 0:
+				api.sendMessage(DNCStrings.ERROR_BAD_USER, sender, null,
+						MessageType.ERROR);
+				
+				return true;
+				// One User Matched
+			case 1:
+				Player p = null;
+				
+				if(sender instanceof Player)
+				{
+					p = (Player) sender;
+				}
+				
+				try
+				{
+					api.changeDisplayName(sender, players[0], players[0].getName());
+					
+					return true;
+				}
+				catch (NonUniqueNickException e)
+				{
+					String sDisplayName = null;
+					
+					if( p != null)
+					{
+						sDisplayName = p.getDisplayName();
+					}
+					
+					log.severe("There was an error reverting from the DisplayName: Display - "
+							+ sDisplayName
+							+ " | BadNick: "
+							+ e.getBadName());
+					
+					return true;
+				}
+				// To Many Users Match
+			default:
+				api.sendMessage(DNCStrings.ERROR_MULTI_MATCH, sender, null,
+						MessageType.ERROR);
+				return true;
+			}
+		}
+		else if (saArgs.length > 1)
+		{
+			api.sendMessage(DNCStrings.ERROR_BAD_ARGS, sender, null,
+					MessageType.ERROR);
+			
+			return false;
+		}
+		return false;
+	}
+	
+	/**
+	 * Gets the maximum number of pages based upon number of entries.
+	 * 
+	 * @param size
+	 *            number of entries total.
+	 * 
+	 * @return the max number of pages.
+	 */
+	private int getMaxPages(int size)
+	{
+		int iReturn = size % plugin.getPagination();
+		
+		if (iReturn == 0)
+		{
+			return size / plugin.getPagination();
+		}
+		else
+		{
+			return (size / plugin.getPagination()) + 1;
+		}
+	}
+	
+	/**
+	 * Gets the player index to start from in the array.
+	 * 
+	 * @param pageNumber
+	 *            the page number requested.
+	 * 
+	 * @param maxPages
+	 *            the max number of pages.
+	 * 
+	 * @return the array index to start listing users from.
+	 */
+	private int getMinPlayerIndex(int pageNumber, int maxPages)
+	{
+		int iIndex;
+		
+		if (pageNumber > maxPages)
+		{
+			iIndex = maxPages;
+		}
+		else
+		{
+			iIndex = pageNumber;
+		}
+		
+		return ((iIndex - 1) * plugin.getPagination());
+	}
+	
+	/**
+	 * Returns an array of Players currently on the server ordered by login
+	 * name.
+	 * 
+	 * @return an array of players.
+	 */
+	private Player[] getPlayerList()
+	{
+		Player[] aPlayers;
+		
+		Collection<Player> collection = plugin.getOrderedPlayers().values();
+		
+		aPlayers = new Player[collection.size()];
+		
+		collection.toArray(aPlayers);
+		
+		return aPlayers;
+	}
+}
